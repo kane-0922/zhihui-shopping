14ac7c7c8fae1694e23ca0388cf22653c8debe05
coffee <2867162687@qq.com>
2026-03-21 14:31:27 +0800

    前期准备

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <div class="box"></div>
+    <!-- 测试代码 -->
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+
+    <van-switch v-model="checked" />
+    <van-rate v-model="score" />
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      checked: true,
+      score: 2
     }
   }
+}
+</script>
+
+<style lang="less">
+.box {
+  width: 300px;
+  height: 300px;
+  background-color: pink;
 }
 </style>
